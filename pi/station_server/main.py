e0f58d46f2ce2ef42bc903bf6ce70a4f4980dd4f
Zhang Cheng <skyxxzc@gmail.com>
2015-07-21 20:34:37 +0800

    usable station cam

--- pi/station_server/main.py
+++ pi/station_server/main.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python
 import flask
 from flask import Flask, Response
-import sys
+import os
 from threading import Thread
 
 camdata = dict()
 
 def reader():
-	while True:
-		cmd = 'raspistill -t 100 -w 100 -h 100 -o img.jpg' #wabout -t
-		with open('img.jpg') as f:
-			camdata['station'] = f.read()
+    while True:
+        try:
+	    cmd = 'raspistill -t 100 -w 100 -h 100 -o img.jpg' #wabout -t
+	    os.system(cmd)
+	    with open('img.jpg') as f:
+		camdata['station'] = f.read()
+        except Exception as e:
+            print e
 			
 app = Flask(__name__,static_url_path='/static')
 @app.route('/')
@@ -19,10 +23,10 @@ def index():
 
 @app.route('/data/cam/<cam>')
 def cam(cam):
-    if cam not in node.camlist: return '-1'
+    if cam not in camdata: return '-1'
     return Response(camdata[cam], mimetype='image/jpeg')
 
 if __name__ == '__main__':
 	r = Thread(target=reader)
 	r.start()
-	app.run(host='0.0.0.0', port=5000, debug=False)
+	app.run(host='0.0.0.0', port=5000, debug=True)
