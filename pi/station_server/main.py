86cc56765295b34cd48ea50da0d5db1c5f388506
Zhang Cheng <skyxxzc@gmail.com>
2015-07-21 17:34:29 +0800

    station

--- pi/station_server/main.py
+++ pi/station_server/main.py
@@ -2,11 +2,27 @@
 import flask
 from flask import Flask, Response
 import sys
+from threading import Thread
 
-img = ''
+camdata = dict()
 
 def reader():
 	while true:
 		cmd = 'raspistill -t 100 -w 100 -h 100 -o img.jpg'
 		with open('img.jpg') as f:
-			img = f.read()
+			camdata['station'] = f.read()
+			
+app = Flask(__name__,static_url_path='/static')
+@app.route('/')
+def index():
+    return app.send_static_file('control.html')
+
+@app.route('/data/cam/<cam>')
+def cam(cam):
+    if cam not in node.camlist: return '-1'
+    return Response(camdata[cam], mimetype='image/jpeg')
+
+if __name__ == '__main__':
+	r = Thread(target=reader)
+	r.start()
+	app.run(host='0.0.0.0', port=5000, debug=False)
